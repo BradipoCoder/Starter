91df989751aef7dc0e0751f5d1de4c576b076e8b
koalacoder <luca.cattaneo@mekit.it>
2016-04-28 10:03:02 +0200

    Clean spaces

--- templates/_node.tpl.php
+++ templates/_node.tpl.php
@@ -18,7 +18,6 @@
   hide($content['links']);
 ?>
 
-
 <div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?>"<?php print $attributes; ?>>
 
   <?php print render($title_prefix); ?>
