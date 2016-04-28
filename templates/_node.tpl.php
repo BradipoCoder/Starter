12b96a6ebbb261f602d2e1654d954efb1bc5f79d
koalacoder <luca.cattaneo@mekit.it>
2016-04-28 10:02:09 +0200

    Better negative ban and _node.tpl-php

--- templates/_node.tpl.php
+++ templates/_node.tpl.php
@@ -15,10 +15,9 @@
 ?>
 
 <?php
-  // We hide the comments and links now so that we can render them later.
-  hide($content['comments']);
   hide($content['links']);
 ?>
+
 
 <div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?>"<?php print $attributes; ?>>
 
